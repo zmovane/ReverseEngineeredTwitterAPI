650895e86b66a49b820056222b1e47e3134a4719
Amovane <amovane@163.com>
2023-07-07 01:37:01 +0800

    feat: add commands

---
+++ src/cmd/mod.rs
@@ -0,0 +1,5 @@
+mod create_space;
+mod mint_nft;
+
+pub use self::create_space::SpaceCreation;
+pub use self::mint_nft::NFTMinting;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::str::FromStr;
+
+use dotenv::dotenv;
+use ethers::types::H160;
+use log::{error, info};
+
+mod cmd;
+mod util;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let client = util::new_client();
+    let command = cmd::NFTMinting {
+        to: H160::from_str("0x8a35A64A20840c71d2eFb5aAeEF6933F5e6A3047").unwrap(),
+        image: "https://imart-nft.s3.us-east-1.amazonaws.com/imart/1684834976.json".to_string(),
+        name: "a tiny boat".to_string(),
+        description: "from Shareverse Bot".to_string(),
+    };
+
+    let result = command.excute(client).await;
+    match result {
+        Ok(_) => {
+            info!("succeed!")
+        }
+        Err(e) => {
+            error!("error: {}", e)
+        }
+    }
 }
