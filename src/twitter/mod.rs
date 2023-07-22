cb0eba3ddf7b06167596c09085cae4cfa18ff9ab
Amovane <amovane@163.com>
2023-07-22 17:44:38 +0800

    chore: test login & search api

--- src/twitter/mod.rs
+++ src/twitter/mod.rs
@@ -1,21 +1,43 @@
-mod api;
-pub use self::api::API;
+mod auth;
+mod search;
+mod types;
 use dotenv::dotenv;
+use reqwest::Client;
+
+pub struct API {
+    client: Client,
+    guest_token: String,
+    csrf_token: String,
+}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{types::Data, *};
 
-    #[tokio::test]
-    async fn test_login() {
+    async fn login(api: &mut API) -> Result<String, String> {
         dotenv().ok();
         let name = std::env::var("TWITTER_USER_NAME").unwrap();
         let pwd = std::env::var("TWITTER_USER_PASSWORD").unwrap();
+        api.login(name, pwd, "".to_string()).await
+    }
+
+    async fn search(api: &mut API) -> Result<Data, reqwest::Error> {
+        let content = "@shareverse_bot -filter:retweets".to_string();
+        let limit = 50;
+        let cursor = "".to_string();
+        api.search(content, limit, cursor).await
+    }
+
+    #[tokio::test]
+    async fn test_login() {
         let mut api = API::new();
-        let result = api.login(name, pwd, "".to_string()).await;
-        assert!(result.is_ok());
+        let loggined = login(&mut api).await;
+        assert!(loggined.is_ok());
 
         let is_logged_in = api.is_logged_in().await;
-        assert!(is_logged_in)
+        assert!(is_logged_in);
+
+        let result = search(&mut api).await;
+        assert!(result.is_ok());
     }
 }
