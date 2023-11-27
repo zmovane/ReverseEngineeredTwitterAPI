b7eb5b9369db2f058f7d204123c3f12d02d49704
ouhuang <1500256797@qq.com>
2023-11-27 18:05:28 +0800

    fix: twitter followers/following api

--- src/relation.rs
+++ src/relation.rs
@@ -11,11 +11,69 @@ use crate::{
 #[async_trait]
 pub trait Relation {
     async fn get_followers(&self, uid: &String) -> Result<FollowersResp, Box<dyn Error>>;
-    async fn get_following(&self, uid: &String) -> Result<FollowingResp, Box<dyn Error>>;
+    async fn get_following(
+        &self,
+        uid: &String,
+        cursor: Option<String>,
+    ) -> Result<FollowingResp, Box<dyn Error>>;
+    async fn check_following(
+        &self,
+        uid: &String,
+        target_uid: &String,
+    ) -> Result<bool, Box<dyn Error>>;
 }
 
 #[async_trait]
 impl Relation for ReAPI {
+    async fn check_following(
+        &self,
+        uid: &String,
+        target_uid: &String,
+    ) -> Result<bool, Box<dyn Error>> {
+        // check whether target_uid is in following list
+        let mut is_following = false;
+        let mut cursor: Option<String> = None;
+        let mut is_continue = true;
+        while is_continue {
+            // if cursor is not empty, then use cursor
+            let res: FollowingResp = self.get_following(uid, cursor.clone()).await?;
+            res.data
+                .user
+                .result
+                .timeline
+                .timeline
+                .instructions
+                .iter()
+                .for_each(|instruction| {
+                    if let Some(entries) = &instruction.entries {
+                        entries.iter().for_each(|entry| {
+                            // if target_uid is == user-xxxxxxx
+                            let target_uid_str = format!("user-{}", target_uid);
+                            if entry.entry_id == target_uid_str {
+                                is_following = true;
+                                is_continue = false;
+                            }
+                            // cursor Type exists
+                            let cursor_type =
+                                &entry.content.cursor_type.clone().unwrap_or_default();
+                            let value = &entry.content.value.clone().unwrap_or_default();
+                            // check whether cursor_type is "Bottom" and the content value is not strat with 0
+                            if cursor_type == "Bottom" && !value.starts_with("0") {
+                                cursor = Some(value.to_string());
+                                is_continue = true;
+                            } else {
+                                is_continue = false;
+                                is_following = false;
+                            }
+                        })
+                    }
+                });
+            // sleep 0.5s
+            tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+        }
+        Ok(is_following)
+    }
+
     async fn get_followers(&self, uid: &String) -> Result<FollowersResp, Box<dyn Error>> {
         let variables = json!(
             {"userId":uid.as_str(),"count":20,"includePromotedContent":false}
@@ -48,7 +106,11 @@ impl Relation for ReAPI {
         return Ok(res);
     }
 
-    async fn get_following(&self, uid: &String) -> Result<FollowingResp, Box<dyn Error>> {
+    async fn get_following(
+        &self,
+        uid: &String,
+        cursor: Option<String>,
+    ) -> Result<FollowingResp, Box<dyn Error>> {
         let mut variables = json!(
             {"userId":uid.as_str(),"count":20,"includePromotedContent":false}
         );
@@ -56,6 +118,10 @@ impl Relation for ReAPI {
             {"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"responsive_web_home_pinned_timelines_enabled":true,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"c9s_tweet_anatomy_moderator_badge_enabled":true,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false}
         );
         variables["product"] = "Latest".into();
+        // check cursor
+        if let Some(c) = cursor {
+            variables["cursor"] = c.as_str().into();
+        }
         let q = [
             ("variables", variables.to_string()),
             ("features", features.to_string()),
@@ -76,8 +142,11 @@ impl Relation for ReAPI {
             .text()
             .await
             .unwrap();
-        let res: FollowingResp = serde_json::from_str(&text).unwrap();
-        return Ok(res);
+        let res = serde_json::from_str(&text);
+        if let Err(e) = res {
+            return Err(Box::new(e));
+        }
+        return Ok(res.unwrap());
     }
 }
 
@@ -106,7 +175,17 @@ mod test_telation {
         let uid = "1439140186378567683".to_string();
         let mut api = ReAPI::new();
         let _loggined = login(&mut api).await;
-        let result = api.get_following(&uid).await;
+        let result = api.get_following(&uid, None).await;
         println!("result {:?}", result);
+    }
+
+    #[tokio::test]
+    async fn test_user_follow_target_user() {
+        let uid = "1439140186378567683".to_string();
+        let target_uid = "1456507428208398336".to_string();
+        let mut api = ReAPI::new();
+        let _loggined = login(&mut api).await;
+        let result = api.check_following(&uid, &target_uid).await;
+        println!("user is following {:?}", result);
     }
 }
--- src/types_resp/following_types.rs
+++ src/types_resp/following_types.rs
@@ -4,337 +4,337 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FollowingResp {
     #[serde(rename = "data")]
-    data: Data,
+    pub data: Data,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Data {
     #[serde(rename = "user")]
-    user: User,
+    pub user: User,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct User {
     #[serde(rename = "result")]
-    result: UserResult,
+    pub result: UserResult,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserResult {
     #[serde(rename = "__typename")]
-    typename: UserDisplayTypeEnum,
+    pub typename: UserDisplayTypeEnum,
 
     #[serde(rename = "timeline")]
-    timeline: ResultTimeline,
+    pub timeline: ResultTimeline,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResultTimeline {
     #[serde(rename = "timeline")]
-    timeline: TimelineTimeline,
+    pub timeline: TimelineTimeline,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TimelineTimeline {
     #[serde(rename = "instructions")]
-    instructions: Vec<Instruction>,
+    pub instructions: Vec<Instruction>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Instruction {
     #[serde(rename = "type")]
-    instruction_type: String,
+    pub instruction_type: String,
 
     #[serde(rename = "direction")]
-    direction: Option<String>,
+    pub direction: Option<String>,
 
     #[serde(rename = "entries")]
-    entries: Option<Vec<Entry>>,
+    pub entries: Option<Vec<Entry>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Entry {
     #[serde(rename = "entryId")]
-    entry_id: String,
+    pub entry_id: String,
 
     #[serde(rename = "sortIndex")]
-    sort_index: String,
+    pub sort_index: String,
 
     #[serde(rename = "content")]
-    content: Content,
+    pub content: Content,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Content {
     #[serde(rename = "entryType")]
-    entry_type: EntryTypeEnum,
+    pub entry_type: EntryTypeEnum,
 
     #[serde(rename = "__typename")]
-    typename: EntryTypeEnum,
+    pub typename: EntryTypeEnum,
 
     #[serde(rename = "itemContent")]
-    item_content: Option<ItemContent>,
+    pub item_content: Option<ItemContent>,
 
     #[serde(rename = "clientEventInfo")]
-    client_event_info: Option<ClientEventInfo>,
+    pub client_event_info: Option<ClientEventInfo>,
 
     #[serde(rename = "value")]
-    value: Option<String>,
+    pub value: Option<String>,
 
     #[serde(rename = "cursorType")]
-    cursor_type: Option<String>,
+    pub cursor_type: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ClientEventInfo {
     #[serde(rename = "component")]
-    component: Component,
+    pub component: Component,
 
     #[serde(rename = "element")]
-    element: Element,
+    pub element: Element,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ItemContent {
     #[serde(rename = "itemType")]
-    item_type: ItemTypeEnum,
+    pub item_type: ItemTypeEnum,
 
     #[serde(rename = "__typename")]
-    typename: ItemTypeEnum,
+    pub typename: ItemTypeEnum,
 
     #[serde(rename = "user_results")]
-    user_results: UserResults,
+    pub user_results: UserResults,
 
     #[serde(rename = "userDisplayType")]
-    user_display_type: UserDisplayTypeEnum,
+    pub user_display_type: UserDisplayTypeEnum,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserResults {
     #[serde(rename = "result")]
-    result: UserResultsResult,
+    pub result: UserResultsResult,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserResultsResult {
     #[serde(rename = "__typename")]
-    typename: UserDisplayTypeEnum,
+    pub typename: UserDisplayTypeEnum,
 
     #[serde(rename = "id")]
-    id: String,
+    pub id: String,
 
     #[serde(rename = "rest_id")]
-    rest_id: String,
+    pub rest_id: String,
 
     #[serde(rename = "affiliates_highlighted_label")]
-    affiliates_highlighted_label: AffiliatesHighlightedLabel,
+    pub affiliates_highlighted_label: AffiliatesHighlightedLabel,
 
     #[serde(rename = "has_graduated_access")]
-    has_graduated_access: bool,
+    pub has_graduated_access: bool,
 
     #[serde(rename = "is_blue_verified")]
-    is_blue_verified: bool,
+    pub is_blue_verified: bool,
 
     #[serde(rename = "profile_image_shape")]
-    profile_image_shape: ProfileImageShape,
+    pub profile_image_shape: ProfileImageShape,
 
     #[serde(rename = "legacy")]
-    legacy: Legacy,
+    pub legacy: Legacy,
 
     #[serde(rename = "professional")]
-    professional: Option<Professional>,
+    pub professional: Option<Professional>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AffiliatesHighlightedLabel {
     #[serde(rename = "label")]
-    label: Option<Label>,
+    pub label: Option<Label>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Label {
     #[serde(rename = "url")]
-    url: LabelUrl,
+    pub url: LabelUrl,
 
     #[serde(rename = "badge")]
-    badge: Badge,
+    pub badge: Badge,
 
     #[serde(rename = "description")]
-    description: String,
+    pub description: String,
 
     #[serde(rename = "userLabelType")]
-    user_label_type: String,
+    pub user_label_type: String,
 
     #[serde(rename = "userLabelDisplayType")]
-    user_label_display_type: String,
+    pub user_label_display_type: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Badge {
     #[serde(rename = "url")]
-    url: String,
+    pub url: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct LabelUrl {
     #[serde(rename = "url")]
-    url: String,
+    pub url: String,
 
     #[serde(rename = "urlType")]
-    url_type: String,
+    pub url_type: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Legacy {
     #[serde(rename = "can_dm")]
-    can_dm: bool,
+    pub can_dm: bool,
 
     #[serde(rename = "can_media_tag")]
-    can_media_tag: bool,
+    pub can_media_tag: bool,
 
     #[serde(rename = "created_at")]
-    created_at: String,
+    pub created_at: String,
 
     #[serde(rename = "default_profile")]
-    default_profile: bool,
+    pub default_profile: bool,
 
     #[serde(rename = "default_profile_image")]
-    default_profile_image: bool,
+    pub default_profile_image: bool,
 
     #[serde(rename = "description")]
-    description: String,
+    pub description: String,
 
     #[serde(rename = "entities")]
-    entities: Entities,
+    pub entities: Entities,
 
     #[serde(rename = "fast_followers_count")]
-    fast_followers_count: i64,
+    pub fast_followers_count: i64,
 
     #[serde(rename = "favourites_count")]
-    favourites_count: i64,
+    pub favourites_count: i64,
 
     #[serde(rename = "followers_count")]
-    followers_count: i64,
+    pub followers_count: i64,
 
     #[serde(rename = "friends_count")]
-    friends_count: i64,
+    pub friends_count: i64,
 
     #[serde(rename = "has_custom_timelines")]
-    has_custom_timelines: bool,
+    pub has_custom_timelines: bool,
 
     #[serde(rename = "is_translator")]
-    is_translator: bool,
+    pub is_translator: bool,
 
     #[serde(rename = "listed_count")]
-    listed_count: i64,
+    pub listed_count: i64,
 
     #[serde(rename = "location")]
-    location: String,
+    pub location: String,
 
     #[serde(rename = "media_count")]
-    media_count: i64,
+    pub media_count: i64,
 
     #[serde(rename = "name")]
-    name: String,
+    pub name: String,
 
     #[serde(rename = "normal_followers_count")]
-    normal_followers_count: i64,
+    pub normal_followers_count: i64,
 
     #[serde(rename = "pinned_tweet_ids_str")]
-    pinned_tweet_ids_str: Vec<String>,
+    pub pinned_tweet_ids_str: Vec<String>,
 
     #[serde(rename = "possibly_sensitive")]
-    possibly_sensitive: bool,
+    pub possibly_sensitive: bool,
 
     #[serde(rename = "profile_banner_url")]
-    profile_banner_url: Option<String>,
+    pub profile_banner_url: Option<String>,
 
     #[serde(rename = "profile_image_url_https")]
-    profile_image_url_https: String,
+    pub profile_image_url_https: String,
 
     #[serde(rename = "profile_interstitial_type")]
-    profile_interstitial_type: String,
+    pub profile_interstitial_type: String,
 
     #[serde(rename = "screen_name")]
-    screen_name: String,
+    pub screen_name: String,
 
     #[serde(rename = "statuses_count")]
-    statuses_count: i64,
+    pub statuses_count: i64,
 
     #[serde(rename = "translator_type")]
-    translator_type: TranslatorType,
+    pub translator_type: TranslatorType,
 
     #[serde(rename = "verified")]
-    verified: bool,
+    pub verified: bool,
 
     #[serde(rename = "want_retweets")]
-    want_retweets: bool,
+    pub want_retweets: bool,
 
     #[serde(rename = "withheld_in_countries")]
-    withheld_in_countries: Vec<Option<serde_json::Value>>,
+    pub withheld_in_countries: Vec<Option<serde_json::Value>>,
 
     #[serde(rename = "url")]
-    url: Option<String>,
+    pub url: Option<String>,
 
     #[serde(rename = "following")]
-    following: Option<bool>,
+    pub following: Option<bool>,
 
     #[serde(rename = "verified_type")]
-    verified_type: Option<Type>,
+    pub verified_type: Option<Type>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Entities {
     #[serde(rename = "description")]
-    description: Description,
+    pub description: Description,
 
     #[serde(rename = "url")]
-    url: Option<Description>,
+    pub url: Option<Description>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Description {
     #[serde(rename = "urls")]
-    urls: Vec<UrlElement>,
+    pub urls: Vec<UrlElement>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UrlElement {
     #[serde(rename = "display_url")]
-    display_url: String,
+    pub display_url: String,
 
     #[serde(rename = "expanded_url")]
-    expanded_url: String,
+    pub expanded_url: String,
 
     #[serde(rename = "url")]
-    url: String,
+    pub url: String,
 
     #[serde(rename = "indices")]
-    indices: Vec<i64>,
+    pub indices: Vec<i64>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Professional {
     #[serde(rename = "rest_id")]
-    rest_id: String,
+    pub rest_id: String,
 
     #[serde(rename = "professional_type")]
-    professional_type: Type,
+    pub professional_type: Type,
 
     #[serde(rename = "category")]
-    category: Vec<Category>,
+    pub category: Vec<Category>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Category {
     #[serde(rename = "id")]
-    id: i64,
+    pub id: i64,
 
     #[serde(rename = "name")]
-    name: String,
+    pub name: String,
 
     #[serde(rename = "icon_name")]
-    icon_name: IconName,
+    pub icon_name: IconName,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
